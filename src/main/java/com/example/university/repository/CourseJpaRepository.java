c7993ee54352c1ad5bc781e2b3ed3124212e5e6e
mallikarjunBurra <mallikarjun9515@gmail.com>
2024-03-29 12:52:39 +0000

    sollution

---
+++ src/main/java/com/example/university/repository/CourseJpaRepository.java
@@ -0,0 +1,28 @@
+/*
+ *
+ * You can use the following import statements
+ * 
+ * import org.springframework.data.jpa.repository.JpaRepository;
+ * import org.springframework.stereotype.Repository;
+ * 
+ */
+
+// Write your code here
+package com.example.university.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+import com.example.university.model.*;
+
+
+import java.util.*;
+
+
+@Repository
+public interface CourseJpaRepository extends JpaRepository<Course, Integer> {
+    List<Course> findByProfessor(Professor professor);
+}
+
